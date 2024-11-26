2c163652fee175ecf25682a938f0ad44fc608242
yesterfriday <qwe50922327@gmail.com>
2024-11-26 22:46:21 +0900

    11.26 1045
    + Background Layer change
    + Camera Ratio change

--- IGP_Project/Assets/Scripts/Enemy/EnemyController.cs
+++ IGP_Project/Assets/Scripts/Enemy/EnemyController.cs
@@ -6,35 +6,37 @@ using Fusion;
 public class EnemyController : NetworkBehaviour
 {
     public float moveSpeed = 3f;
-    private bool isMovingRight = true;
+
+    [Networked] // 네트워크에서 동기화할 변수
+    public bool isMovingRight { get; set; } = true; // 기본값을 true로 설정
 
     private SpriteRenderer sr;
     private Rigidbody2D rb2d;
-    void Start()
+
+    void Awake()
     {
-        rb2d  = GetComponent<Rigidbody2D>();
+        rb2d = GetComponent<Rigidbody2D>();
         sr = GetComponent<SpriteRenderer>();
         sr.flipX = true;
     }
 
-
     public override void FixedUpdateNetwork()
     {
-        if (isMovingRight)
-        {
-            rb2d.velocity = new Vector2(moveSpeed, rb2d.velocity.y);
-            sr.flipX = true;
-        }
-        else
-        {
-            rb2d.velocity = new Vector2(-moveSpeed, rb2d.velocity.y);
-            sr.flipX = false;
-        }
+        Vector2 newVelocity = new Vector2(isMovingRight ? moveSpeed : -moveSpeed, rb2d.velocity.y);
+        rb2d.velocity = newVelocity;
+        sr.flipX = isMovingRight; // flipX 상태 업데이트
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.CompareTag("Boundary"))
-            isMovingRight = !isMovingRight;
+        if (collision.CompareTag("Boundary"))
+        {
+            // 상태 권한이 있는 경우에만 이동 방향 변경
+            if (HasStateAuthority)
+            {
+                // 방향을 반전시키고 이 변경을 네트워크에 동기화
+                isMovingRight = !isMovingRight;
+            }
+        }
     }
 }
